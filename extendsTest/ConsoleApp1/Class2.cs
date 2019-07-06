a55f0f7fe6650f951c9932a218e3af2443d6a569
yangxiaoxu <17857318857@163.com>
2019-07-06 22:02:25 +0800

    对继承的调用测试

---
+++ extendsTest/ConsoleApp1/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class2 : Class1
+    {
+
+            //public string a = "init2";
+            public Class2()
+            {
+            }
+
+            public void Fun()
+            {
+                Console.WriteLine("C2:" + base.a);
+            }
+     }
+}
---
+++ extendsTest/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            Class2 c2 = new Class2();
+            //Console.WriteLine(c2.a);
+            c2.Fun();
+            c2.Fun1();
+            c2.Fun();
+            c2.Fun2();
+            c2.Fun();
+        }
+
+    }
+
+}
